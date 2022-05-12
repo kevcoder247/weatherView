7ca25cbb61dd3b83f6a48620e78402d031f3f766
)- <a-@-s-MacBook-Pro.local>
2022-05-11 21:01:16 -0400

    api call can now target specific adata and place it on our page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 
+//VARIABLES
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=New%20york,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646";
+const $temp = $('#temp');
+const $name = $('#name');
+const $high = $('#high');
+const $low = $('#low');
 
-const URL = "https://api.openweathermap.org/data/2.5/weather?q=New%20york,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646"
+
+
+//ELEMENT REFERENCES
+
+//EVENT LISTENERS
+
+//FUNCTIONS
+
 function handleGetData(){
     $.ajax(URL).then(function (data) {
         console.log('weather is ready')
         console.log(data)
+        $temp.text(data.main.temp);
+        $name.text(data.name);
+        $high.text(data.main.temp_max);
+        $low.text(data.main.temp_min);
     }, function(error){
         console.log('something is wrong');
         console.log(error)
