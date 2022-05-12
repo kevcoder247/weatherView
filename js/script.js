fb541234522a8f4b4ebc6a2cd19bccf03b47630b
)- <a-@-s-MacBook-Pro.local>
2022-05-11 20:14:02 -0400

    added html and js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
 
 const URL = "https://api.openweathermap.org/data/2.5/weather?q=New%20york,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646"
+function handleGetData(){
+    $.ajax(URL).then(function (data) {
+        console.log('weather is ready')
+        console.log(data)
+    }, function(error){
+        console.log('something is wrong');
+        console.log(error)
+    })
+}
 
-$.ajax(URL).then(function (data) {
-    console.log('weather is ready')
-    console.log(data)
-})
