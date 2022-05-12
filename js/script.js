4f4c6d40e4844ba65c111f1a7dee6229905e889e
)- <a-@-s-MacBook-Pro.local>
2022-05-12 11:10:35 -0400

    api string fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
 //VARIABLES
-const URL = "https://api.openweathermap.org/data/2.5/weather?q=New%20york,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646";
+//working api link=====================
+// const URL = "https://api.openweathermap.org/data/2.5/weather?q=New%20york,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646";
+//====================================
+//Figure out how to modify the api when input is sumbitted so the right call is made when different cities are inputted
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=APPID=8f3fc16f81290ad9120f68d8c7b94646";
+// https://api.openweathermap.org/data/2.5/weather?q=boston,us&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646
+                
 const $temp = $('#temp');
 const $name = $('#name');
 const $high = $('#high');
@@ -17,8 +23,16 @@ $form.on('submit', handleGetData)
 
 //FUNCTIONS
 
-function handleGetData(){
-    $.ajax(URL).then(function (data) {
+function link(query){
+    return encodeURI(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&APPID=8f3fc16f81290ad9120f68d8c7b94646`);
+}
+
+function handleGetData(event){
+    
+    event.preventDefault()
+    const userInput = $input.val();
+
+    $.ajax(link(userInput)).then(function (data) {
         console.log('weather is ready')
         console.log(data)
         $temp.text(data.main.temp);
@@ -31,5 +45,5 @@ function handleGetData(){
     })
 }
 
-handleGetData();
+
 
